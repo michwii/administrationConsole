cf952ddd733b6527a42fa6f454dbd71c12060fc1
Michwii <freezerhm@yahoo.fr>
2017-07-06 21:05:19 +0200

    Minimum code for the sliders + bootstrap

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 app.use('/cnc', panTilt);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
