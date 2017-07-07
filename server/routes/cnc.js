8385648822e4064656b2f37bafe9a88bf85eef27
Michwii <freezerhm@yahoo.fr>
2017-07-07 23:04:02 +0200

    Adding instruction instructions

--- server/routes/cnc.js
+++ server/routes/cnc.js
@@ -6,21 +6,11 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.post('/X_axis', function (req, res) {
-  com(JSON.stringify(req.body), function(feedback){
-    res.json(feedback);
-  });
-});
-router.post('/A_axis', function (req, res) {
-  com(JSON.stringify(req.body), function(feedback){
-    res.json(feedback);
-  });
-});
-
-router.post('/Y_axis', function (req, res) {
-  com(JSON.stringify(req.body), function(feedback){
-    res.json(feedback);
-  });
+router.post('/command', function (req, res) {
+  //com(JSON.stringify(req.body), function(feedback){
+    //res.json(feedback);
+  //});
+  res.json(req.body);
 });
 
 module.exports = router;
